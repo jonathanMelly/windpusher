d16ec741c5cbc249504bb024872f45cd2d7f632c
Jonathan Melly <jonathan.melly@gmail.com>
2023-08-30 09:29:22 +0200

    feat(config): allow multiple stations...

--- main.py
+++ main.py
@@ -4,59 +4,68 @@ import os
 import time
 import re
 import requests
+import toml
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
-from dotenv import load_dotenv
-
 from requests_toolbelt.utils import dump
 
+# load config file
+with open('config.toml', 'r') as f:
+    config = toml.loads(f.read())
+
+# Start browser
 options = Options()
 options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
 
+# Handles stations
 try:
-    load_dotenv()
+    for stationName in config:
+        station = config[stationName]
+        driver.get(station["src"])
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
 
-    driver.get('https://letskite.ch/spots/31/kitesurf/baie-dallaman')
-    html = driver.page_source
-    soup = BeautifulSoup(html,"html.parser")
+        blockWarning = soup.find("div", {"id": "warning"})
+        if len(blockWarning.find()) > 0:
+            print(stationName + " seems out of order, discarding update")
+            continue
 
-    blockMesure = soup.find("div", {"id": "block-mesure"})
+        measureBlock = soup.find("div", {"id": "block-mesure"})
+        numbers = re.compile(r"\d+")
 
-    numbers = re.compile(r"\d+")
+        wind = numbers.findall(measureBlock.find("div", {"id": "vent_vitesse"}).text).pop()
+        windGust = numbers.findall(measureBlock.find("div", {"id": "vent_rafale"}).text).pop()
+        windDirection = numbers.findall(measureBlock.find("div", {"id": "vent_direction"}).text).pop()
 
-    wind = numbers.findall(blockMesure.find("div", {"id": "vent_vitesse"}).text).pop()
-    windGust = numbers.findall(blockMesure.find("div", {"id": "vent_rafale"}).text).pop()
-    windDirection = numbers.findall(blockMesure.find("div", {"id": "vent_direction"}).text).pop()
+        print("station:" + stationName + "\nwind:" + wind + "\ngust:", windGust + "\ndir:", windDirection)
 
-    print("wind:"+wind+"\ngust:", windGust+"\ndir:", windDirection)
+        apiUrl = station["target"]
 
-    wgUrl = "http://www.windguru.cz/upload/api.php"
+        # location given here
+        salt = time.time()
+        uid = station["uid"]
+        password = station["pwd"]
 
-    # location given here
-    salt = time.time()
-    uid = os.getenv("UID")
-    password = os.getenv("PASSWORD")
+        wgHash = hashlib.md5((str(salt) + uid + password).encode()).hexdigest()
 
-    wghash = hashlib.md5((str(salt) + uid + password).encode()).hexdigest()
+        # defining a params dict for the parameters to be sent to the API
+        apiParams = {'uid': uid,
+                     'salt': salt,
+                     'hash': wgHash,
+                     'wind_avg': wind,
+                     'wind_max': windGust,
+                     'wind_direction': windDirection
+                     }
 
-    # defining a params dict for the parameters to be sent to the API
-    apiParams = {'uid': uid,
-                 'salt': salt,
-                 'hash': wghash,
-                 'wind_avg': wind,
-                 'wind_max': windGust,
-                 'wind_direction': windDirection
-                 }
-
-    # sending get request and saving the response as response object
-    r = requests.get(url=wgUrl, params=apiParams)
-
-    print(dump.dump_all(r))
-    print(r)
+        # sending get request and saving the response as response object
+        r = requests.get(url=apiUrl, params=apiParams)
+        print(r)
+        if os.getenv("DEBUG"):
+            print(dump.dump_all(r))
 
 finally:
     driver.close()
